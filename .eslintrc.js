e6c3a94fde885edf1d1cea7b2361e4768ad6d886
Diego Stratta <strattadb@gmail.com>
2017-03-25 03:33:05 -0300

    Add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  "root": true,
+  "env": {
+    "node": true,
+    "es6": true,
+    "commonjs": true,
+  },
+  "extends": "eslint:recommended",
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "sourceType": "module",
+    "allowImportExportEverywhere": true,
+  },
+  "rules": {
+    "indent": ["error", 2, { "SwitchCase": 1 }],
+    "linebreak-style": ["error", "unix"],
+    "quotes": ["error", "single"],
+    "semi": ["error", "always"],
+    "no-extra-semi": "error",
+    "no-console": 0,
+    "comma-dangle": ["error", "always-multiline"],
+    "eol-last": ["error", "always"],
+    "max-len": ["error", {
+      "code": 80,
+      "ignoreStrings": true,
+      "ignoreTemplateLiterals": true,
+      "ignoreRegExpLiterals": true,
+    }],
+    "no-trailing-spaces": "error",
+    "space-before-function-paren": ["error", "always"],
+    "spaced-comment": ["error", "always"],
+    "func-call-spacing": ["error", "never"],
+    "arrow-parens": ["error", "always"],
+  },
+};
