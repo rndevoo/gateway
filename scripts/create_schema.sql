c1adb166c63c2ea80cb71edd9878070befc09559
Diego Stratta <strattadb@gmail.com>
2017-03-26 00:34:44 -0300

    Create 'users' SQL table

---
+++ scripts/create_schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+  id              bigserial    PRIMARY KEY,
+  username        varchar(40)  NOT NULL UNIQUE,
+  first_name      varchar(40)  NOT NULL,
+  last_name       varchar(40)  NOT NULL,
+  email           varchar(254) NOT NULL UNIQUE,
+  password        varchar(512) NOT NULL,
+  created         timestamp    NOT NULL,
+)
+
+CREATE TABLE one_on_one_meetings (
+  id              bigserial    PRIMARY KEY,
+)
