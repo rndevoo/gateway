53a1407557129ebd5e089d9e1dec5a2fd3897ab8
Diego Stratta <strattadb@gmail.com>
2017-03-26 21:02:16 -0300

    Rename create_schema.sql to create_tables.sql

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -5,9 +5,9 @@ CREATE TABLE users (
   last_name       varchar(40)  NOT NULL,
   email           varchar(254) NOT NULL UNIQUE,
   password        varchar(512) NOT NULL,
-  created         timestamp    NOT NULL,
-)
+  created         timestamp    NOT NULL
+);
 
 CREATE TABLE one_on_one_meetings (
-  id              bigserial    PRIMARY KEY,
-)
+  id              bigserial    PRIMARY KEY
+);
