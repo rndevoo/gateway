43772cab768554bbb842eff0e3a98876c06665ba
Diego Stratta <strattadb@gmail.com>
2017-03-28 02:01:26 -0300

    Update create_tables.sql

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,13 +1,24 @@
 CREATE TABLE users (
-  id              bigserial    PRIMARY KEY,
-  username        varchar(40)  NOT NULL UNIQUE,
-  first_name      varchar(40)  NOT NULL,
-  last_name       varchar(40)  NOT NULL,
-  email           varchar(254) NOT NULL UNIQUE,
-  password        varchar(512) NOT NULL,
-  created         timestamp    NOT NULL
+  id               bigserial    PRIMARY KEY,
+  username         varchar(40)  NOT NULL UNIQUE,
+  first_name       varchar(40)  NOT NULL,
+  last_name        varchar(40)  NOT NULL,
+  email            varchar(254) NOT NULL UNIQUE,
+  password         varchar(512) NOT NULL,
+  is_active        boolean      NOT NULL DEFAULT FALSE,
+  is_admin         boolean      NOT NULL DEFAULT FALSE,
+  created          timestamp    NOT NULL
 );
 
-CREATE TABLE one_on_one_meetings (
-  id              bigserial    PRIMARY KEY
+CREATE TYPE gender_identities AS ENUM ();
+
+CREATE TABLE profiles (
+  user_id          bigint       REFERENCES users UNIQUE,
+  bio              varchar(512),
+  birth_date       timestamp,
+  gender_identity  gender_identities
 );
+
+CREATE TABLE settings (
+  user_id          bigint       REFERENCES users UNIQUE
+)
