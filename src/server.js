c084a9b80907f2015a19372ba562e248fea4ba5e
Diego Stratta <strattadb@gmail.com>
2017-03-30 03:17:20 -0300

    Add logger and bodyparser middlewares

--- src/server.js
+++ src/server.js
@@ -1,5 +1,16 @@
 'use strict';
 
 import Koa from 'koa';
+import logger from 'koa-logger';
+import bodyParser from 'koa-bodyparser';
 
 const app = new Koa();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(logger());
+app.use(bodyParser());
+
+app.listen(PORT, () => {
+  console.log(`LetsMeet server running on port: ${PORT}`);
+});
