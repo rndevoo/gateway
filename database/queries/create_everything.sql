b8d56ef172e0430bc4d2c7b1af15cad4db769105
Diego Stratta <strattadb@gmail.com>
2017-03-30 02:36:28 -0300

    Add database init script

---
+++ database/queries/create_everything.sql
@@ -0,0 +1,29 @@
+CREATE TABLE users (
+  id               bigserial       PRIMARY KEY,
+  username         varchar(40)     NOT NULL UNIQUE,
+  first_name       varchar(40)     NOT NULL,
+  last_name        varchar(40)     NOT NULL,
+  email            varchar(254)    NOT NULL UNIQUE,
+  password         varchar(512)    NOT NULL,
+  is_active        boolean         NOT NULL DEFAULT FALSE,
+  is_admin         boolean         NOT NULL DEFAULT FALSE,
+  created          timestamp       NOT NULL
+);
+
+CREATE TYPE gender_identity AS ENUM (
+  'agender', 'androgyne', 'bigender', 'genderqueer', 'non-binary',
+  'gender bender', 'hijra', 'pangender', 'queer heterosexuality',
+  'third gender', 'trans man', 'trans woman', 'transmasculine',
+  'transfemenine', 'trigender', 'two-spirit'
+);
+
+CREATE TABLE profiles (
+  user_id          bigint          REFERENCES users ON DELETE CASCADE UNIQUE,
+  bio              varchar(512),
+  birth_date       timestamp,
+  gender_identity  gender_identity
+);
+
+CREATE TABLE settings (
+  user_id          bigint          REFERENCES users ON DELETE CASCADE UNIQUE
+);
