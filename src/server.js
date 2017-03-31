39ea68156ca834e8e89b068b36af8db089f34e3c
Diego Stratta <strattadb@gmail.com>
2017-03-30 22:04:58 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,12 +4,17 @@ import Koa from 'koa';
 import logger from 'koa-logger';
 import bodyParser from 'koa-bodyparser';
 
+import router from './router/router';
+
 const app = new Koa();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(logger());
-app.use(bodyParser());
+app
+  .use(logger())
+  .use(bodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(PORT, () => {
   console.log(`LetsMeet server running on port: ${PORT}`);
