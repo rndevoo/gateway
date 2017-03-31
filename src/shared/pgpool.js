dbd928200acf056aeb26f4789997b3d0731a1d1b
Diego Stratta <strattadb@gmail.com>
2017-03-31 09:30:41 -0300

    Change const names

--- src/shared/pgpool.js
+++ src/shared/pgpool.js
@@ -2,20 +2,12 @@
 
 import Pool from 'pg-pool';
 
-const DATABASE_USER = 'diego';
-const DATABASE_NAME = 'letsmeet';
-const DATABASE_PASSWORD = 'asdf';
-const DATABASE_HOST = 'localhost';
-const DATABASE_PORT = 5432;
-const MAX_POOL_CLIENTS = 10;
-const IDLE_TIMEOUT_MS = 1000;
-
 const config = {
-  user: process.env.DATABASE_USER || DATABASE_USER,
-  database: process.env.DATABASE_NAME || DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD || DATABASE_PASSWORD,
-  host: process.env.DATABASE_HOST || DATABASE_HOST,
-  port: process.env.DATABASE_PORT || DATABASE_PORT,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   max: MAX_POOL_CLIENTS,
   idleTimeoutMillis: IDLE_TIMEOUT_MS,
 };
