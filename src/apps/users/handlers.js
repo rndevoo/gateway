1824f4640935fc86317a84281aa157038224a1d9
Diego Stratta <strattadb@gmail.com>
2017-04-02 01:29:27 -0300

    Implement users handlers

---
+++ src/apps/users/handlers.js
@@ -0,0 +1,38 @@
+'use strict';
+
+import { User } from './models/user';
+
+export class UserHandlers {
+  static async list (ctx) {
+    const options = {
+      showProfile: ctx.query.show_profile,
+      showPreferences: ctx.query.show_preferences,
+    };
+    ctx.body = await User.list(options);
+  }
+
+  static async retrieve (ctx) {
+    const { id } = ctx.params;
+    const fields = [
+      'username',
+      'first_name',
+      'birth_date',
+      'gender_identity',
+    ];
+    const result = await User.retrieve('id', id, fields);
+
+    if (!result) {
+      ctx.status = 404;
+      return;
+    }
+
+    ctx.body = result;
+  }
+
+  static async delete (ctx) {
+    const { id } = ctx.params;
+    const success = await User.delete('id', id);
+
+    ctx.status = success ? 202 : 404;
+  }
+}
