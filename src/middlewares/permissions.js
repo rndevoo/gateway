a728a065b0ee6e9023fb23fb11135a88e5291fdf
Diego Stratta <strattadb@gmail.com>
2017-04-02 01:27:02 -0300

    Implement isAdmin() permission

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -1,13 +1,20 @@
 'use strict';
 
 export async function isAdminOrSelf (ctx, next) {
-
+  // TODO: figure out a way to determine if user is himself
+  return next();
 }
 
 export async function isAdmin (ctx, next) {
-
+  const isAdmin = ctx.state.user.is_admin;
+  if (isAdmin) {
+    return next();
+  } else {
+    ctx.status = 401;
+  }
 }
 
 export async function isSelf (ctx, next) {
-
+  // TODO: figure out a way to determine if user is himself
+  return next();
 }
