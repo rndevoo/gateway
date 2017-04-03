a04a0fea601cca2464a24dce37b9550402186b3d
Diego Stratta <strattadb@gmail.com>
2017-04-03 00:09:58 -0300

    Add new routes

--- src/router/router.js
+++ src/router/router.js
@@ -4,11 +4,15 @@ import Router from 'koa-router';
 
 import usersRouter from './../apps/users/router';
 import authRouter from './../apps/auth/router';
+import activationRouter from './../apps/activation/router';
+import registrationRouter from './../apps/registration/router';
 
 const router = new Router({ prefix: '/api/v1' });
 
 router
   .use('/users', usersRouter.routes(), usersRouter.allowedMethods())
-  .use('/auth', authRouter.routes(), authRouter.allowedMethods());
+  .use('/auth', authRouter.routes(), authRouter.allowedMethods())
+  .use('/activation', activationRouter.routes(), activationRouter.allowedMethods())
+  .use('/registration', registrationRouter.routes(), registrationRouter.allowedMethods());
 
 export default router;
