39bbabaf11e40803e60d5ea459b5b6b7727830bf
Diego Stratta <strattadb@gmail.com>
2017-04-03 00:09:47 -0300

    Improve profile model code quality

--- src/apps/users/models/profile.js
+++ src/apps/users/models/profile.js
@@ -3,31 +3,17 @@
  */
 'use strict';
 
-import pool from './../../../shared/pgpool';
+import pool from './../../../../lib/pgpool';
 
 export class Profile {
   /**
    * Creates a new profile for an existing user.
    *
-   * @param {String} getBy - The field to identify the user by.
-   * @param {(String|Number)} value - The value to identify the user with.
+   * @param {Number} userId - The user's id.
    *
-   * @returns {Boolean} Returns true if succeeded and false otherwise.
+   * @returns {Boolean}  True if succeeded and false otherwise.
    */
-  static async create (getBy, value) {
-    let userId;
-    if (getBy === 'id') {
-      userId = value;
-    } else {
-      const sqlGetUserIdString = `
-      SELECT id
-      FROM users
-      WHERE ${getBy} = $1;
-      `;
-
-      const users = await pool.query(sqlGetUserIdString, [value]);
-      userId = users.rows[0].id;
-    }
+  static async create (userId) {
     const sqlString = `
     INSERT INTO profiles (user_id)
     VALUES ($1);
