be96668a458d8f88b295201c2105834c16e7af6c
Diego Stratta <strattadb@gmail.com>
2017-04-04 11:51:26 -0300

    Remove old Postgres stuff

--- database/create_user_preferences_table.sql
+++
@@ -1,9 +0,0 @@
-----------------------------------------------------------------------------
-------------------------- USER PREFERENCES TABLE ---------------------------
-----------------------------------------------------------------------------
-
-DROP TABLE IF EXISTS user_preferences CASCADE;
-
-CREATE TABLE user_preferences (
-  user_id          integer         REFERENCES users ON DELETE CASCADE UNIQUE
-);
