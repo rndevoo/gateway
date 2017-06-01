dc1056fd65c28b8dae2712f2e6551bc7e6503934
Diego Stratta <strattadb@gmail.com>
2017-05-31 21:58:00 -0300

    Add GraphiQL route

--- src/router.ts
+++ src/router.ts
@@ -3,15 +3,22 @@
  * The main router.
  */
 
-import Router from 'koa-router';
-import graphql from 'graphql-server-koa';
+import * as Router from 'koa-router';
+import { graphqlKoa, graphiqlKoa } from 'graphql-server-koa';
 
 import schema from './schema';
 
 const router = new Router();
 
 router
-  .get('/graphql', graphql({ schema }))
-  .post('/graphql', graphql({ schema }));
+  .get('/graphql', graphqlKoa({ schema }))
+  .post('/graphql', graphqlKoa({ schema }));
+
+const NODE_ENV = process.env.NODE_ENV;
+
+// We only want GraphiQL in development.
+if (NODE_ENV === 'development') {
+  router.get('/graphiql', graphiqlKoa({ endpointURL: '/graphql' }));
+}
 
 export default router;
