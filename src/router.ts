6755b81bcbd741de83e466f31d1724eff24e5b41
Diego Stratta <strattadb@gmail.com>
2017-06-02 03:18:12 -0300

    Use function variants of graphqlKoa middleware

--- src/router.ts
+++ src/router.ts
@@ -6,13 +6,19 @@
 import * as Router from 'koa-router';
 import { graphqlKoa, graphiqlKoa } from 'graphql-server-koa';
 
-import schema from './schema';
+import GraphQLSchema from './schema';
 
 const router = new Router();
 
 router
-  .get('/graphql', graphqlKoa({ schema }))
-  .post('/graphql', graphqlKoa({ schema }));
+  .get('/graphql', graphqlKoa((ctx) => ({
+    schema: GraphQLSchema,
+    context: { ch: ctx.state.channel },
+  })))
+  .post('/graphql', graphqlKoa((ctx) => ({
+    schema: GraphQLSchema,
+    context: { ch: ctx.state.channel },
+  })));
 
 const NODE_ENV = process.env.NODE_ENV;
 
